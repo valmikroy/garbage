18480d632a0f7b53f31072deeb2c85968d499831
Valmik Roy <valmikroy@gmail.com>
2016-01-27 21:55:19 +0000

    number printing and divisiblity rule

---
+++ practise/printing_name_numbers.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env  ruby
+
+# if divisible by 4 then Linked
+
+# if divisible by 6 then In
+
+# divisible by both then LinkedIn
+
+a = 20
+b = 18
+
+c = (a*b/(a-b))
+
+
+
+( 1 .. 1000 ).each do |n|  
+
+	# figured a rule 
+	# (6*4/(6-4)) == 12 
+	# if number divisible by 12 then it should be divisible by both
+	puts "LinkedIn #{n}" if n%c == 0
+	puts "Linked #{n}" if n%b == 0
+	puts "In #{n}" if n%a == 0
+	puts n 
+
+end
