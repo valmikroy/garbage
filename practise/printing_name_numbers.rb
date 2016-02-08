aab3b19da7452ec006e3999ca0f832c56429eef9
git <abhisawa@yahoo-inc.com>
2016-02-08 08:22:12 -0800

    Older way of doing things

--- practise/printing_name_numbers.rb
+++ practise/printing_name_numbers.rb
@@ -6,24 +6,35 @@
 
 # divisible by both then LinkedIn
 
-a = 20
-b = 18
+#a = 20
+#b = 18
+#
+#c = (a*b/(a-b))
+#
 
-c = (a*b/(a-b))
+
+#( 1 .. 1000 ).each do |n|  
+#	# figured a rule 
+#	# (6*4/(6-4)) == 12 
+#	# if number divisible by 12 then it should be divisible by both
+#	if n%c == 0 
+#		puts "LinkedIn #{n}"
+#	elsif  n%b == 0
+#		puts "Linked #{n}"
+#	elsif  n%a == 0
+#		puts "In #{n}"
+#	else
+#		puts n
+#	end
+#end
 
 
 
-( 1 .. 1000 ).each do |n|  
-	# figured a rule 
-	# (6*4/(6-4)) == 12 
-	# if number divisible by 12 then it should be divisible by both
-	if n%c == 0 
-		puts "LinkedIn #{n}"
-	elsif  n%b == 0
-		puts "Linked #{n}"
-	elsif  n%a == 0
-		puts "In #{n}"
-	else
-		puts n
-	end
+
+(1 .. 100).each do |n|
+	puts
+	print "Linked" if n%6 == 0 
+	print "In" if n%4 == 0 
+	print n if n%6 != 0 and n%4 != 0
 end
+
