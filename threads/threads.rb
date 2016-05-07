102ad9f71b34ba5dce5eafb73012aa3ef886707c
git <abhisawa@yahoo-inc.com>
2016-05-07 13:20:53 -0700

    Adding threads only and thread pool approch

--- threads/threads.rb
+++ threads/threads.rb
@@ -12,29 +12,38 @@ def exec!(*command)
 	cmd
 end
 
-def dumb
+# Simple function
+def dumb(x)
 
-	word = 'blah'
-	url="https://www.google.com/?gws_rd=ssl#q=#{word}"
-	exec!("curl -i /dev/null '#{url}'")
+	#word = 'blah'
+	#url="https://www.google.com/?gws_rd=ssl#q=#{word}"
+	#exec!("curl -i /dev/null '#{url}'")
+    [128000+x].pack "U*"
+    #print " #{x} " 
 end
 	
 
-
+# Repeat some task for 1000 times
 def threadpool
 
 
+    # work queue defines 1000 times iteration
 	work_q = Queue.new
-	(0...99).to_a.each { |i| work_q << i }
+	(0...999).to_a.each { |i| work_q << i }
 
 	t = []
 	puts Benchmark.measure{
+        # we spawn only 5 threads
 		t = (0...4).map do 
 			Thread.new do
 				begin 
+                    # before taking any action 
+                    # thread try to pop from work queue
+                    # if work queue is empty then 
+                    # threaderror exception get raised
 					while x = work_q.pop(true)
 						#puts x
-						50.times{ dumb  }
+						dumb(x) 
 					end
 				rescue ThreadError
 				end
@@ -44,3 +53,24 @@ def threadpool
 	}
 
 end
+
+def threads_only
+
+    t = []
+    puts Benchmark.measure {
+        # plain 1000 threads 
+        1000.times do |x|
+            t << Thread.new do
+                dumb(x)
+            end    
+        end
+        t.each { |x| x.join }
+    }
+
+end
+
+
+
+threadpool
+
+threads_only
