58fcb116939b70f68276c6b25349eb14b719b8dd
Valmik Roy <valmikroy@users.noreply.github.com>
2020-12-02 10:12:33 -0800

    Update fibonacci.rb

--- practise/fibonacci.rb
+++ practise/fibonacci.rb
@@ -1,22 +1,41 @@
 #!/usr/bin/env ruby
 
 
-require 'pp'
+# n      0	1	2	3	4	5	6	7	8	9	10	11	12	13	14	15 
+# fib(n) 0	1	1	2	3	5	8	13	21	34	55	89	144	233	377	610
 
-( 0 .. 20 ).inject(Array.new) do |a,b|
+def fib(n)
+    
+    seq = []
+    idx = 0
+    loop do 
+        
+        if idx == 0 
+           seq.push(0) 
+        elsif idx == 1
+           seq.push(1) 
+        else
+            seq[idx] = seq[idx-1] + seq[idx-2]
+        end
 
-	if a.length < 2
-		a.push(b)
-	else
+        break if idx == n
+        idx +=1
+    end
 
-		
-		last = a.shift
-		o_last = a.shift
-		now = last + o_last
-		a = [ now , last ]
-		puts now
-	end
-	a
+    return seq
+
 end
 
 
+def fib_req(n)
+    return 0 if n == 0
+    return 1 if n == 1
+    ret = fib_req(n-1) + fib_req(n-2)
+    return ret
+end
+
+puts "Without recursion"
+puts fib(15)
+puts
+puts "With recursion"
+puts fib_req(14)
